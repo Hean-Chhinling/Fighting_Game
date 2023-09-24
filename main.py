3357f1d2a3935d2e8ac54282eca12368b0927363
Hean Chhinling <92643868+Hean-Chhinling@users.noreply.github.com>
2023-09-24 14:53:18 +0200

    Update game title

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Letter Fighting")
+pygame.display.set_caption("Cat Fighting")
 
 # set frame rate
 clock = pygame.time.Clock()
